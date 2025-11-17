cdc8620db4e44fd2b3f772e18ab4e3d2bf395a7d
RomPri03 <romainprieur2810@gmail.com>
2025-11-17 15:31:53 +0100

    Test a reproduire

---
+++ tp3.1.py
@@ -0,0 +1,7 @@
+for i in range(5):
+    print(i)
+
+x = 0
+while x < 10:
+    x += 1
+    print(x)
---
+++ tp3.2.py
@@ -0,0 +1,6 @@
+nums = [1, 2, 3]
+nums.append(4)
+print(nums)
+
+info = {"nom": "Bob", "age": 30}
+print(info["nom"])
